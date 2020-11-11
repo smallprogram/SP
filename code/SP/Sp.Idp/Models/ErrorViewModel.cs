cb7976d78f0fa134d06229b5befb4ed55e4ee8fc
zhusir <smallprogram@foxmail.com>
2020-11-11 15:46:01 +0800

    update

---
+++ code/SP/Sp.Idp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sp.Idp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
